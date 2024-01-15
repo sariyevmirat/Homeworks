1de5091e91caad5f6e80b9497d98f06fcacfafaf
sariyevmirat <150445699+sariyevmirat@users.noreply.github.com>
2024-01-15 16:37:44 +0500

    del

--- Practicalwork100124/main.py
+++
@@ -1,8 +0,0 @@
-print("Hello world")
-input()
-
-script_directory = os.path.dirname(os.path.abspath(sys.argv[0]))
-def chwd(path):
-    if os.path.exists(path):
-        os.chdir(path)
-chwd(script_directory)
